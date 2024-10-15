fcc2851a2ab08622f766e71317b56f4cf8ae9a65
IsakBH <89648535+IsakBH@users.noreply.github.com>
2024-10-15 19:39:12 +0200

    Oppdater ting

---
+++ Stylesheets/C++/neimenhallaisenverden.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // An array storing different ages
+    int ages[8] = {20, 22, 18, 35, 48, 26, 87, 70};
+
+    float avg, sum = 0;
+    int i;
+
+    // Get the length of the array
+    int length = sizeof(ages) / sizeof(ages[0]);
+
+    // Loop through the elements of the array
+    for (int age : ages)
+    {
+        sum += age;
+    }
+
+    // Calculate the average by dividing the sum by the length
+    avg = sum / length;
+
+    // Print the average
+    cout << "The average age is: " << avg << "\n";
+}
+
+    // hallo hallo det e han addexio
