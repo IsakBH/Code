ed18f741023d814de09f21462a2e791485c40b89
IsakBH <89648535+IsakBH@users.noreply.github.com>
2024-10-27 22:09:39 +0100

    Legg til PHP
    har begynt å leke litt med php
    virker mye kulere enn javascript

---
+++ php/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+  <title> kul php </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+</head>
+<body>
+<h2>kul php ting</h2>
+<form action="index.php" method="post">
+    <label>username: </label>
+    <input type="text" name="username"> <br>
+    <label>password: </label>
+    <input type="password" name="password"> <br>
+    <input type="submit" name="login" value="Log in"> <br>
+</form>
+</body>
+</html>
+
+<?php /*if (isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    if (empty($username)) {
+        echo "Username is not filled in";
+    } elseif (empty($password)) {
+        echo "Password is not filled in";
+    } else {
+        echo "Welcome $username";
+    }
+}
+*/
+foreach ($_POST as $key => $value) {
+    echo "$key = $value <br>";
+}
+
+?>
