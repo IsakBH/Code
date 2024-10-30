573c815d3013a6239de652bfd56d0f8d24e3ac1f
isakbh <isak@brunhenriksen.net>
2024-10-30 10:05:24 +0100

    Leker rundt med kryptering av passord og hasher og sånn

--- php/index.php
+++ php/index.php
@@ -1,26 +1,37 @@
 <!DOCTYPE html>
 <html lang="no">
+
 <head>
-  <title> kul php </title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="styling.css">
+    <title> kul php </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styling.css">
 </head>
+
 <body>
-<h2>Kul PHP</h2>
+    <h2>Kul PHP</h2>
+    <form action="index.php" method="post">
+        <label>Skriv inn passord:</label> <br>
+        <input type="password" id="input" name="password" value="password"> <br>
+        <input type="submit" id="skibidi" name="submit" value="confirm"> <br>
+    </form>
 </body>
+
 </html>
 
 <?php
-function is_even($number)
-{
-    // Lager en funksjon som heter "is_even" med parameteret "$number". Parameteret er noe du må skrive inn i parantesen når du caller på funksjonen, og oppfører seg som en midlertidig variabel som bare eksisterer inni funksjonen.
-    return $number % 2; // Returnerer variabelen "$number" med modulus 2.
+$password = null;
+
+if (isset($_POST["submit"])) {
+    $password = $_POST["password"];
+    $encrypted = password_hash($password, PASSWORD_DEFAULT, ["cost" => 10]);
+    echo "Encrypted password: $encrypted <br>";
 }
 
-echo is_even(11);
-
-// printer resultatet av "is_even" funksjonen med "$number" parameteret som 11.
-
+if (password_verify($password, $encrypted)) {
+    echo "Password is valid. <br>";
+} else {
+    echo "Password is invalid. You shall not pass! <br>";
+}
 
 ?>
