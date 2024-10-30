978e0f8a31513183a89c611fcbea06591af234b7
isakbh <isak@brunhenriksen.net>
2024-10-30 13:56:30 +0100

    Driver med kule HTML form ting

--- php/index.php
+++ php/index.php
@@ -13,25 +13,56 @@
     <form action="index.php" method="post">
         <label>Skriv inn passord:</label> <br>
         <input type="password" id="input" name="password" value="password"> <br>
-        <input type="submit" id="skibidi" name="submit" value="confirm"> <br>
+        <input type="submit" id="skibidi" name="submit1" value="submit"> <br>
+    </form> <br>
+    <form action="index.php" method="post">
+        <label>Hvilken informasjon vil du ha?</label> <br>
+        <input type="checkbox" id="input" name="httpinfos[]" value="HTTP_HOST"> HTTP_HOST <br>
+        <input type="checkbox" id="input" name="httpinfos[]" value="HTTP_REFERER"> HTTP_REFERER <br>
+        <input type="checkbox" id="input" name="httpinfos[]" value="HTTP_USER_AGENT"> HTTP_USER_AGENT <br>
+        <input type="submit" id="skibidi" name="submit2" value="submit"> <br>
     </form>
 </body>
 
 </html>
 
 <?php
-$password = null;
+$password = null; // Lager en variabel med verdi null.
+$httpinfos = $_POST["httpinfos"];
 
-if (isset($_POST["submit"])) {
-    $password = $_POST["password"];
-    $encrypted = password_hash($password, PASSWORD_DEFAULT, ["cost" => 10]);
-    echo "Encrypted password: $encrypted <br>";
+if (isset($_POST["submit1"])) { // Sjekker om "submit" knappen i et HTML-form er set, eller, trykket på.
+    $password = $_POST["password"]; // Setter verdien til $password variabelen til samme verdien som "password" inputten fra HTML-formet
+    $encrypted = password_hash($password, PASSWORD_DEFAULT, ["cost" => 10]); // Lag en variabel med password_hash, som encrypter $password variabelen med PASSWORD_DEFAULT encryption, som akkurat nå er bcrypt. "cost" vil si hvor mye CPU den får lov til å bruke, eller, hvor mye det "koster" av CPU-en å encrypte passordet.
+    echo "Encrypted password: $encrypted <br>"; // printer ut det krypterte passordet i en string.
 }
 
-if (password_verify($password, $encrypted)) {
+if (password_verify($password, $encrypted)) { // Sjekker om password_verify kan verifisere at $password og $encrypted er make. Den decrypter $encrypted og matcher resultatet med $password.
     echo "Password is valid. <br>";
-} else {
-    echo "Password is invalid. You shall not pass! <br>";
 }
 
+if (isset($_POST["submit2"])) {
+    $httpinfos = $_POST["httpinfos"];
+
+    foreach ($httpinfos as $httpinfo) {
+        echo "$httpinfo <br>";
+    }
+}
+
+/*if (isset($_POST["submit2"])) {
+    switch ($httpinfo) {
+        case "HTTP_HOST";
+            echo $_SERVER["HTTP_HOST"];
+            break;
+
+        case "HTTP_REFERER";
+            echo $_SERVER["HTTP_REFERER"];
+            break;
+        case "HTTP_USER_AGENT";
+            echo $_SERVER["HTTP_USER_AGENT"];
+            break;
+        default:
+            echo "Velg informasjonen du vil ha";
+    }
+}
+*/
 ?>
