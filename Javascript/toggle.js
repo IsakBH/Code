eccdcc96c78c59ef8c76046c936a72bd1aca5cba
IsakBH <isak@brunhenriksen.net>
2024-11-16 17:04:04 +0100

    Fikset darkmode på logg siden, skal fikse på andre sider også senere.

--- Javascript/toggle.js
+++ Javascript/toggle.js
@@ -1,4 +1,9 @@
 function myFunction() {
-    var element = document.body;
-    element.classList.toggle("light-mode");
-} 
+    const toggleButton = document.getElementById('togglelight');
+    const body = document.body;
+
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('darkmode');
+        body.classList.toggle('lightmode');
+    });
+}
--- index.php
+++ index.php
@@ -19,38 +19,9 @@
 
     <h1> Isak B. Henriksen </h1>
 
-    <div id="biography">
-        <div id="bio">
-            <p id="bio2">
-                Informasjonsteknologi og Medieproduksjon elev på Amalieskram VGs, litt nerd, Linux bruker og profesjonell i
-                nesten alle videospill.
-            </p>
-        </div>
-    </div>
-
-    <div id="isak">
-        <p>
-            Veldig interresert i alt data (maskinvare, programvare, osv) og har drevet med det ganske lenge. Jeg spiller også ganske mye
-            spill av alle sjangre, men mest skytespill. Jeg synes også at ting relatert til
-            servere, databaser og internett er veldig kult. Hvis du vil spille Overwatch, er det bare å si hop on og så blir jeg med.
-        </p>
-    </div>
-
-    <div id="isak2">
-        <p>
-            Jeg kommer fra Bergen og bor i Loddefjord. Siden jeg fortsatt går på skole, har jeg ingen profesjonelle
-            prestasjoner eller mål. Jeg har 100% achievements og nesten 2600 timer spilletid i Cookie Clicker på Steam,
-            som jeg vil si betyr at jeg er dedikert og god på det jeg er interessert i. Grunnen til at jeg valgte IM-linjen er at jeg har vært veldig interessert i IT hele livet, og synes at media delen også er litt interessant.
-        </p>
-    </div>
-
-    <div id="kontakt">
-        <ul>
-            <li> <ion-icon name="mail"></ion-icon>isak@brunhenriksen.net </li>
-            <li> 📞 458 48 234 </li>
-            <li> <ion-icon name="logo-discord"></ion-icon> isakbh </li>
-        </ul>
-    </div>
+<?php
+include("Logg/Text/ommeg.txt");
+?>
 
     <div class="side2knapp">
         <button onclick="location.href = '/Undersider/side2.php';" id="myButton"
