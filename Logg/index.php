610238b93c9fb1f5d1222896e999a5892d727212
isakbh <isak@brunhenriksen.net>
2024-12-04 11:12:43 +0100

    Ny logg + styling greier

--- Logg/index.php
+++ Logg/index.php
@@ -17,41 +17,36 @@ include("../Include/HTML/navbar.html");
 <body>
     <div class="content">
         <div class="sidebar">
+            <a href="#2.12.24">2.12.24</a>
             <a href="#28.11.24">28.11.24</a>
             <a href="#27.11.24">27.11.24</a>
             <a href="#15.11.24">15.11.24</a>
             <a href="#dokumentasjon">Dokumentasjon</a>
         </div>
         <div id="logg">
-            <h2 id="28.11.24">28.11.24 (dobbeltime)</h2>
+            <h2 id?="2.12.24">Mandag 2.12.24 (dobbeltime) </h2>
+            <?php
+            include("../Logg/Text/logg-02.12.24.txt");
+            ?>
+            <br> <hr> <br>
+
+            <h2 id="28.11.24">Torsdag 28.11.24 (dobbeltime)</h2>
             <?php
             include("../Logg/Text/logg-28.11.24.txt");
             ?>
-            <br>
+            <br> <hr> <br>
 
-            <hr>
-
-            <br>
-
-            <h2 id="27.11.24">27.11.24 (dobbeltime)</h2>
+            <h2 id="27.11.24">Onsdag 27.11.24 (dobbeltime)</h2>
             <?php
             include("../Logg/Text/logg-27.11.24.txt");
             ?>
-            <br>
+            <br> <hr> <br>
 
-            <hr>
-
-            <br>
-
-            <h2 id="15.11.24">15.11.24 (dobbeltime)</h2>
+            <h2 id="15.11.24">Fredag 15.11.24 (dobbeltime)</h2>
             <?php
             include("../Logg/Text/logg-15.11.24.txt");
             ?>
-            <br>
-
-            <hr>
-
-            <br>
+            <br> <hr> <br>
 
             <h1 id="dokumentasjon">Dokumentasjon</h1>
             <?php
