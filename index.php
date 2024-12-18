398e8d2d11758ed8fce5d9c5f71deffef2332374
isakbh <isak@brunhenriksen.net>
2024-12-18 12:32:09 +0100

    Lagt til Google Maps embed på om meg siden

--- index.php
+++ index.php
@@ -27,6 +27,9 @@ include("Logg/Text/ommeg.txt");
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<br>
+
+<iframe id="maps" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d150.92522395422816!2d5.233279111964471!3d60.3660681255376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sno!4v1734516022151!5m2!1sen!2sno" width="800" height="650" style="border:0;" allowfullscreen="yes" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 </body>
 
